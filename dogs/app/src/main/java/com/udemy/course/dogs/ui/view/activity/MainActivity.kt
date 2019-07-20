2f0811de954848162b0a81e788ced8e063689349
Elias Meireles <eliasmflilico@gmail.com>
2019-07-19 21:06:44 -0300

    Navigation with arguments between fragments.

--- dogs/app/src/main/java/com/udemy/course/dogs/ui/view/activity/MainActivity.kt
+++ dogs/app/src/main/java/com/udemy/course/dogs/ui/view/activity/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.udemy.course.dogs.ui.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.udemy.course.dogs.R
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
--- dogs/app/src/main/java/com/udemy/course/dogs/ui/view/fragment/DetailFragment.kt
+++ dogs/app/src/main/java/com/udemy/course/dogs/ui/view/fragment/DetailFragment.kt
@@ -5,15 +5,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.udemy.course.dogs.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
+
+    private var dogUuid = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        arguments?.let {
+            dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
+            Toast.makeText(view.context, "$dogUuid", Toast.LENGTH_LONG).show()
+        }
+
+        floatingActionButtonList.setOnClickListener {
+            val actionDetailFragment = DetailFragmentDirections.actionListFragment()
+            Navigation.findNavController(it).navigate(actionDetailFragment)
+        }
+    }
 }
