c6e15925e5a09c80baead4e2de33542c118266c5
Elias Meireles <eliasmflilico@gmail.com>
2019-07-19 21:11:50 -0300

    Clean up example code.

--- dogs/app/src/main/java/com/udemy/course/dogs/ui/view/fragment/DetailFragment.kt
+++ dogs/app/src/main/java/com/udemy/course/dogs/ui/view/fragment/DetailFragment.kt
@@ -5,11 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import com.udemy.course.dogs.R
-import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
 
@@ -25,12 +22,6 @@ class DetailFragment : Fragment() {
 
         arguments?.let {
             dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
-            Toast.makeText(view.context, "$dogUuid", Toast.LENGTH_LONG).show()
-        }
-
-        floatingActionButtonList.setOnClickListener {
-            val actionDetailFragment = DetailFragmentDirections.actionListFragment()
-            Navigation.findNavController(it).navigate(actionDetailFragment)
         }
     }
 }
--- dogs/app/src/main/java/com/udemy/course/dogs/ui/view/fragment/ListFragment.kt
+++ dogs/app/src/main/java/com/udemy/course/dogs/ui/view/fragment/ListFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import com.udemy.course.dogs.R
-import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
@@ -16,15 +14,5 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        floatingActionButtonDetails.setOnClickListener {
-            val actionDetailFragment = ListFragmentDirections.actionDetailFragment()
-            actionDetailFragment.dogUuid = 5
-            Navigation.findNavController(it).navigate(actionDetailFragment)
-        }
     }
 }
