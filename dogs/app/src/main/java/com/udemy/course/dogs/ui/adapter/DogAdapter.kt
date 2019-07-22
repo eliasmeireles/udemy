3473cd48ab3ad9fb5e1508a1502143edb08b87aa
Elias Meireles <eliasmflilico@gmail.com>
2019-07-22 19:56:51 -0300

    DataBiding...

--- dogs/app/src/main/java/com/udemy/course/dogs/ui/adapter/DogAdapter.kt
+++ dogs/app/src/main/java/com/udemy/course/dogs/ui/adapter/DogAdapter.kt
@@ -3,18 +3,23 @@ package com.udemy.course.dogs.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.udemy.course.dogs.R
+import com.udemy.course.dogs.databinding.ItemDogBinding
 import com.udemy.course.dogs.model.Dog
 import com.udemy.course.dogs.ui.view.fragment.ListFragmentDirections
+import com.udemy.course.dogs.ui.view.listener.DogClickListener
 import com.udemy.course.dogs.util.getProgressDrawable
 import com.udemy.course.dogs.util.loadImage
 import kotlinx.android.synthetic.main.item_dog.view.*
 
-class DogAdapter(val dogList: ArrayList<Dog>) : RecyclerView.Adapter<DogAdapter.DogViewHolder>() {
+class DogAdapter(val dogList: ArrayList<Dog>) : RecyclerView.Adapter<DogAdapter.DogViewHolder>(), DogClickListener {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_dog, parent, false)
+        val inflater = LayoutInflater.from(parent.context)
+        val itemView =
+            DataBindingUtil.inflate<ItemDogBinding>(inflater, R.layout.item_dog, parent, false)
         return DogViewHolder(itemView)
     }
 
@@ -25,19 +30,18 @@ class DogAdapter(val dogList: ArrayList<Dog>) : RecyclerView.Adapter<DogAdapter.
         this.dogList.addAll(dogList)
         notifyDataSetChanged()
     }
+
     override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
-        holder.itemView.dogName.text = dogList[position].dogBreed
-        holder.itemView.lifespan.text = dogList[position].lifeSpan
-        holder.itemView.dogImage.loadImage(dogList[position].imageUrl, getProgressDrawable(holder.itemView.context))
-        holder.itemView.setOnClickListener {
-            val actionDetailFragment = ListFragmentDirections.actionDetailFragment()
-            actionDetailFragment.dogUuid = dogList[position].uuid.toInt()
-            Navigation.findNavController(it).navigate(actionDetailFragment)
-        }
+        holder.view.dog = dogList[position]
+        holder.view.listener = this
     }
 
 
-    class DogViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+    override fun onDogClicked(view: View) {
+        val actionDetailFragment = ListFragmentDirections.actionDetailFragment()
+            actionDetailFragment.dogUuid = view.dogId.text.toString().toInt()
+            Navigation.findNavController(view).navigate(actionDetailFragment)
     }
+
+    class DogViewHolder(var view: ItemDogBinding) : RecyclerView.ViewHolder(view.root)
 }
---
+++ dogs/app/src/main/java/com/udemy/course/dogs/ui/view/listener/DogClickListener.kt
@@ -0,0 +1,7 @@
+package com.udemy.course.dogs.ui.view.listener
+
+import android.view.View
+
+interface DogClickListener {
+    fun onDogClicked(view: View)
+}
